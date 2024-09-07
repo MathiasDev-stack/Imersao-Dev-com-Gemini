0a66ba129c3a83563b49aa53176c871e2766e28e
Evandro Mathias <119955079+MathiasDev-stack@users.noreply.github.com>
2024-09-07 03:32:19 -0300

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+let section = document.getElementById("resultado-pesquisa");
+
+section.innerHTML = `
+  <div class="item-pesquisa">
+    <h2>Lucas Moura</h2>
+    <p>Lucas Moura: Um craque brasileiro de renome internacional
+      Lucas Rodrigues Moura da Silva, mais conhecido como Lucas Moura, é um talentoso futebolista brasileiro que já vestiu as camisas de grandes clubes como São Paulo, Paris Saint-Germain e Tottenham Hotspur. Atualmente, voltou a defender as cores do São Paulo, clube onde iniciou sua carreira profissional. Conhecido por sua velocidade, dribles e habilidade de finalização, Lucas Moura se destacou em diversas competições, conquistando títulos importantes e encantando os fãs do futebol com suas jogadas brilhantes. Sua trajetória no futebol o consolidou como um dos maiores jogadores brasileiros da sua geração.</p>
+    <a href="https://pt.wikipedia.org/wiki/Lucas_Moura" target="_blank">Mais informações</a>
+  </div>
+`;
